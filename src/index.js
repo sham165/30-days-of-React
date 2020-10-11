c1063d3706ffbc364691077525861c3f6d6b1552
Sham165 <src165@gmail.com>
2020-10-11 15:13:39 -0400

    apply an internal style

--- src/index.js
+++ src/index.js
@@ -1,56 +1,59 @@
+// index.js
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const jsxElement = <h1> This is a JSX element</h1>;
-// const title = <h2>Getting Started with React</h2>;
-
-const headerStyle = {
+const headerStyles = {
   backgroundColor: "#61DBFB",
   fontFamily: "Helvetica Neue",
   padding: 25,
   lineHeight: 1.5,
 };
 
+// JSX element, header
 const header = (
-  <header style={headerStyle}>
+  <header style={headerStyles}>
     <div className="header-wrapper">
-      <h1>Welcome to 30 days of React</h1>
+      <h1>Welcome to 30 Days Of React</h1>
       <h2>Getting Started React</h2>
       <h3>JavaScript Library</h3>
-      <p>Shams C</p>
-      <small>Oct 11th 2020</small>
+      <p>Student: Shams C</p>
+      <small>Date: Oct 2, 2020</small>
     </div>
   </header>
 );
 
+// JSX element, main
 const mainStyles = {
   backgroundColor: "#F3F0F5",
 };
 const main = (
   <main style={mainStyles}>
-    <p>
-      {" "}
-      Prerequisite to get started react.js:
-      <ui>
+    <div className="main-wrapper">
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
         <li>HTML</li>
         <li>CSS</li>
-        <li>Javascript</li>
-      </ui>
-    </p>
+        <li>JavaScript</li>
+      </ul>
+    </div>
   </main>
 );
 
-const footerStyle = {
+const footerStyles = {
   backgroundColor: "#61DBFB",
 };
+// JSX element, footer
 const footer = (
-  <footer style={footerStyle}>
-    <p>Copyright 2020</p>
+  <footer style={footerStyles}>
+    <div className="footer-wrapper">
+      <p>Copyright 2020</p>
+    </div>
   </footer>
 );
 
+// JSX element, app
 const app = (
-  <div classname="app">
+  <div className="app">
     {header}
     {main}
     {footer}
@@ -58,5 +61,5 @@ const app = (
 );
 
 const rootElement = document.getElementById("root");
-
+// we render the JSX element using the ReactDOM package
 ReactDOM.render(app, rootElement);
