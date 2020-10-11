d67272e1bf4ae367cceefab493ee5067b946d31a
Sham165 <src165@gmail.com>
2020-10-11 15:46:21 -0400

    injecting data to jsx elements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const welcome = "Welcome to 30 Days Of React";
+const title = "Getting Started React";
+const subtitle = "JavaScript Library";
+const author = {
+  firstName: "Shams",
+  lastName: "C",
+};
+const date = "Oct 2, 2020";
 const headerStyles = {
   backgroundColor: "#61DBFB",
   fontFamily: "Helvetica Neue",
@@ -13,14 +21,39 @@ const headerStyles = {
 const header = (
   <header style={headerStyles}>
     <div className="header-wrapper">
-      <h1>Welcome to 30 Days Of React</h1>
-      <h2>Getting Started React</h2>
-      <h3>JavaScript Library</h3>
-      <p>Student: Shams C</p>
-      <small>Date: Oct 2, 2020</small>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Student: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
     </div>
   </header>
 );
+
+const numOne = 1;
+const numTwo = 2;
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+);
+
+const yearBorn = 1995;
+const currentYear = new Date().getFullYear();
+const age = currentYear - yearBorn;
+const personAge = (
+  <p>
+    {" "}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+);
+
+// JSX element, main
+const techs = ["HTML", "CSS", "JavaScript"];
+const techsFormatted = techs.map((tech) => <li>{tech}</li>);
 
 // JSX element, main
 const mainStyles = {
@@ -29,16 +62,21 @@ const mainStyles = {
 const main = (
   <main style={mainStyles}>
     <div className="main-wrapper">
-      <p>Prerequisite to get started react.js:</p>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-      </ul>
+      <p>
+        Prerequisite to get started
+        <strong>
+          <em>react.js</em>
+        </strong>
+        :
+      </p>
+      <ul>{techsFormatted}</ul>
+      {result}
+      {personAge}
     </div>
   </main>
 );
 
+const copyRight = "Copyright 2020";
 const footerStyles = {
   backgroundColor: "#61DBFB",
 };
@@ -46,7 +84,7 @@ const footerStyles = {
 const footer = (
   <footer style={footerStyles}>
     <div className="footer-wrapper">
-      <p>Copyright 2020</p>
+      <p>{copyRight}</p>
     </div>
   </footer>
 );
