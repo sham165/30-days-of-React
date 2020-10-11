4b035be1ddd65990e4cd2215e2813b2805f48cea
Sham165 <src165@gmail.com>
2020-10-11 14:47:35 -0400

    added inline styles to each elements

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const jsxElement = <h1> This is a JSX element</h1>;
+// const title = <h2>Getting Started with React</h2>;
+
+const headerStyle = {
+  backgroundColor: "#61DBFB",
+  fontFamily: "Helvetica Neue",
+  padding: 25,
+  lineHeight: 1.5,
+};
+
+const header = (
+  <header style={headerStyle}>
+    <div className="header-wrapper">
+      <h1>Welcome to 30 days of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Shams C</p>
+      <small>Oct 11th 2020</small>
+    </div>
+  </header>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const mainStyles = {
+  backgroundColor: "#F3F0F5",
+};
+const main = (
+  <main style={mainStyles}>
+    <p>
+      {" "}
+      Prerequisite to get started react.js:
+      <ui>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+      </ui>
+    </p>
+  </main>
+);
+
+const footerStyle = {
+  backgroundColor: "#61DBFB",
+};
+const footer = (
+  <footer style={footerStyle}>
+    <p>Copyright 2020</p>
+  </footer>
+);
+
+const app = (
+  <div classname="app">
+    {header}
+    {main}
+    {footer}
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(app, rootElement);
