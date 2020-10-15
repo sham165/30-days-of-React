8c0d35da2c45f34c3b0eaf45bd073a27b456da95
Sham165 <src165@gmail.com>
2020-10-15 09:51:35 -0400

    Mapping array of objects

--- src/index.js
+++ src/index.js
@@ -24,7 +24,6 @@ const showDate = (time) => {
   const date = time.getDate();
   return ` ${month} ${date}, ${year}`;
 };
-
 // JSX element, header
 const Header = ({
   data: {
@@ -49,19 +48,16 @@ const Header = ({
     </header>
   );
 };
-
 const PersonAge = ({ yearBorn }) => {
   const currentYear = new Date().getFullYear();
   const age = currentYear - yearBorn;
   return <p> I am {age} years old</p>;
 };
-
 // TechList Component
 const TechList = ({ techs }) => {
   const techList = techs.map((tech) => <li key={tech}>{tech}</li>);
   return techList;
 };
-
 // User Card Component
 const UserCard = ({ user: { firstName, lastName, image } }) => (
   <div className="user-card">
@@ -71,14 +67,12 @@ const UserCard = ({ user: { firstName, lastName, image } }) => (
     </h2>
   </div>
 );
-
 // A button component
 const Button = ({ text, onClick, style }) => (
   <button style={style} onClick={onClick}>
     {text}
   </button>
 );
-
 // CSS styles in JavaScript Object
 const buttonStyle = {
   backgroundColor: "#61dbfb",
@@ -90,7 +84,6 @@ const buttonStyle = {
   fontSize: 18,
   color: "white",
 };
-
 // HexaColor component
 const HexaColor = () => {
   let str = "0123456788abcdef";
@@ -101,9 +94,30 @@ const HexaColor = () => {
   }
   return "#" + color;
 };
+// Country component
+const Country = ({ country: { name, city } }) => {
+  return (
+    <div>
+      <h4>{name}</h4>
+      <small>City: {city}</small>
+    </div>
+  );
+};
+// countries component
+const Countries = ({ countries }) => {
+  const countryList = countries.map((country) => <Country country={country} />);
+  return <div>{countryList}</div>;
+};
 
 // main component
-const Main = ({ user, techs, greetPeople, handleTime, yearBorn }) => (
+const Main = ({
+  user,
+  techs,
+  greetPeople,
+  handleTime,
+  yearBorn,
+  countries,
+}) => (
   <main>
     <div className="main-wrapper">
       <p>
@@ -116,9 +130,12 @@ const Main = ({ user, techs, greetPeople, handleTime, yearBorn }) => (
       <ul>
         <TechList techs={techs} />
       </ul>
+      <div>
+        <h3>Countries List</h3>
+        <Countries countries={countries} />
+      </div>
       <UserCard user={user} />
       <PersonAge yearBorn={yearBorn} />
-
       <Button text="Show Time" onClick={handleTime} style={buttonStyle} />
       <Button text="Greet Plople" onClick={greetPeople} style={buttonStyle} />
       <Button
@@ -131,11 +148,10 @@ const Main = ({ user, techs, greetPeople, handleTime, yearBorn }) => (
           {" "}
           <HexaColor />
         </p>
-      </strong>
+      </strong>{" "}
     </div>
   </main>
 );
-
 // footer component
 const Footer = (copyRight) => (
   <footer>
@@ -166,6 +182,14 @@ const App = () => {
 
   const techs = ["HTML", "CSS", "JavaScript"];
 
+  const countries = [
+    { name: "Finland", city: "Helsinki" },
+    { name: "Sweden", city: "Stockholm" },
+    { name: "Denmark", city: "Copenhagen" },
+    { name: "Norway", city: "Oslo" },
+    { name: "Iceland", city: "Reykjavík" },
+  ];
+
   // copying the author from data object to user variable using spread operator
   const user = { ...data.author, image: image };
 
@@ -185,6 +209,7 @@ const App = () => {
         handleTime={handleTime}
         greetPeople={greetPeople}
         yearBorn={yearBorn}
+        countries={countries}
       />
       <Footer copyRight={date} />
     </div>
