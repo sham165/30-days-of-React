08b392d158c72d8cf560d8599df95dfe5c820397
Sham165 <src165@gmail.com>
2020-10-24 12:16:47 -0400

    higher order component

--- src/index.js
+++ src/index.js
@@ -1,360 +1,98 @@
-import React, { useState } from "react";
-import ReactDom from "react-dom";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-const options = [
-  {
-    value: "",
-    label: "-- Select a Country --",
-  },
-  {
-    value: "Mexico",
-    label: "Mexico",
-  },
-  {
-    value: "USA",
-    label: "USA",
-  },
-  {
-    value: "Canada",
-    label: "Canada",
-  },
-  {
-    value: "Denmark",
-    label: "Denmar",
-  },
-];
-
-//mapping the options to list(array) of jsx options
-
-const selectOptions = options.map(({ value, label }) => (
-  <option key={label} value={value}>
-    {" "}
-    {label}
-  </option>
-));
-
-const App = (props) => {
-  const initialState = {
-    firstName: "",
-    lastName: "",
-    country: "",
-    title: "",
-    tel: "",
-    dateOfBirth: "",
-    favoriteColor: "",
-    weight: "",
-    gender: "",
-    file: "",
-    bio: "",
-    skills: {
-      html: false,
-      css: false,
-      javascript: false,
-    },
-    touched: {
-      firstName: false,
-      lastName: false,
-    },
-  };
-
-  const [formData, setFormData] = useState(initialState);
-
-  const onChange = (e) => {
-    /*
-    We can get the name and value like: e.target.name, e.target.value
-    We can also destructure name and value from e.target
-    const name = e.target.name
-    const value = e.target.value
-    */
-    const { name, value, type, checked } = e.target;
-    /*
-    [variablename] we can make a value stored in a certain variable could be a key for an object, in this case a key for the state
-    */
-
-    if (type === "checkbox") {
-      setFormData({
-        ...formData,
-        skills: { ...formData.skills, [name]: checked },
-      });
-    } else if (type === "file") {
-      setFormData({ ...formData, [name]: e.target.files[0] });
-    } else {
-      setFormData({ ...formData, [name]: value });
-    }
-  };
-
-  const onSubmit = (e) => {
-    /* 
-     e.preventDefault()
-      stops the default behavior of form element
-     specifically refreshing of page
-     */
-    e.preventDefault();
-    const {
-      firstName,
-      lastName,
-      title,
-      email,
-      tel,
-      dateOfBirth,
-      favoriteColor,
-      weight,
-      country,
-      gender,
-      bio,
-      file,
-      skills,
-    } = formData;
-
-    const formattedSkills = [];
-    for (const key in skills) {
-      console.log(key);
-      if (skills[key]) {
-        formattedSkills.push(key.toUpperCase());
-      }
-    }
-
-    const data = {
-      firstName,
-      lastName,
-      title,
-      email,
-      tel,
-      dateOfBirth,
-      favoriteColor,
-      weight,
-      country,
-      gender,
-      bio,
-      file,
-      skills: formattedSkills,
-    };
-
-    /*
-     the is the place where we connect backend api 
-     to send the data to the database
-     */
-    console.log(data);
-  };
-
-  const onBlur = (e) => {
-    const { name } = e.target;
-    setFormData({
-      ...formData,
-      touched: { ...formData.touched, [name]: true },
-    });
-  };
-  const validate = () => {
-    // Obbject to coolect error feedback and to display on the form
-    const errors = {
-      firstName: "",
-    };
-    if (
-      (formData.touched.firstName && formData.firstName.length < 3) ||
-      (formData.touched.firstName && formData.firstName.length > 12)
-    ) {
-      errors.firstName = "First name must be between 2 and 12";
-    }
-    return errors;
-  };
-
-  // accessing the state value by destrutcturing the state
-  const {
-    firstName,
-    lastName,
-    title,
-    country,
-    email,
-    tel,
-    dateOfBirth,
-    favoriteColor,
-    weight,
-    gender,
-    bio,
-  } = formData;
-
-  const errors = validate();
+const Button = ({ onClick, text, style }) => {
   return (
-    <div className="App">
-      <h3>Add Student</h3>
-
-      <form onSubmit={onSubmit}>
-        <div className="row">
-          <div className="form-group">
-            <label htmlFor="firstName">First Name </label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              placeholder="First Name"
-              value={firstName}
-              onChange={onChange}
-              onBlur={onBlur}
-            />
-            <br />
-            {errors.file && <small>{errors.firstName}</small>}
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastName">Last Name </label>
-            <input
-              type="text"
-              name="lastName"
-              placeholder="Last Name"
-              value={lastName}
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="title">Title</label>
-            <input
-              type="text"
-              name="title"
-              placeholder="Title"
-              value={title}
-              onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={onChange}
-              placeholder="Email"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="tel">Telephone </label>
-            <input
-              type="tel"
-              id="tel"
-              name="tel"
-              value={tel}
-              onChange={onChange}
-              placeholder="Tel"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="dateOfBirth">Date of birth </label>
-            <input
-              type="date"
-              id="dateOfBirth"
-              name="dateOfBirth"
-              value={dateOfBirth}
-              onChange={onChange}
-              placeholder="Date of Birth"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="favoriteColor">Favorite Color</label>
-            <input
-              type="color"
-              id="color"
-              name="favoriteColor"
-              value={favoriteColor}
-              onChange={onChange}
-              placeholder="Favorite Color"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="weight">Weight </label>
-            <input
-              type="number"
-              id="weight"
-              name="weight"
-              value={weight}
-              onChange={onChange}
-              placeholder="Weight in Kg"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="coutry">Country</label> <br />
-            <select
-              name="country"
-              id="country"
-              value={country}
-              onChange={onChange}
-            >
-              {selectOptions}
-            </select>
-          </div>
-
-          <div>
-            <p>Gender</p>
-            <div>
-              <input
-                type="radio"
-                id="female"
-                name="gender"
-                value="Female"
-                onChange={onChange}
-                checked={gender === "Female"}
-              />
-              <label htmlFor="female">Female</label>
-            </div>
-            <div>
-              <input
-                id="male"
-                type="radio"
-                name="gender"
-                value="Male"
-                onChange={onChange}
-                checked={gender === "Male"}
-              />
-              <label htmlFor="male">Male</label>
-            </div>
-            <div>
-              <input
-                id="other"
-                type="radio"
-                name="gender"
-                value="Other"
-                onChange={onChange}
-                checked={gender === "Other"}
-              />
-              <label htmlFor="other">Other</label>
-            </div>
-
-            <div>
-              <p>Select your skills</p>
-              <div>
-                <input type="checkbox" id="html" onChange={onChange} />
-                <label htmlFor="html">HTML</label>
-              </div>
-              <div>
-                <input type="checkbox" id="css" onChange={onChange} />
-                <label htmlFor="css">HTML</label>
-              </div>
-              <div>
-                <input type="checkbox" id="javascript" onChange={onChange} />
-                <label htmlFor="html">avaScript</label>
-              </div>
-            </div>
-            <div>
-              <label htmlFor="bio">Bio</label> <br />
-              <textarea
-                id="bio"
-                name="bio"
-                value={bio}
-                onChange={onChange}
-                cols="120"
-                rows="10"
-                placeholder="Write about yourself ..."
-              />
-            </div>
-
-            <div>
-              <input type="file" name="file" onChange={onChange} />
-            </div>
-          </div>
-          <button className="btn btn-success">Submit</button>
-        </div>
-      </form>
-    </div>
+    <button className="button" onClick={onClick} style={style}>
+      {text}
+    </button>
   );
 };
 
+const buttonWithStyles = (CopmParam, name = "default") => {
+  const colors = [
+    {
+      name: "default",
+      backgroundColor: "#e7e7e7",
+      color: "#000000",
+    },
+    {
+      name: "react",
+      backgroundColor: "#61dbfb",
+      color: "#ffffff",
+    },
+    {
+      name: "success",
+      backgroundColor: "#4CAF50",
+      color: "#ffffff",
+    },
+    {
+      name: "info",
+      backgroundColor: "#2196F3",
+      color: "#ffffff",
+    },
+    {
+      name: "warning",
+      backgroundColor: "#ff9800",
+      color: "#ffffff",
+    },
+    {
+      name: "danger",
+      backgroundColor: "#f44336",
+      color: "#ffffff",
+    },
+  ];
+  const { backgroundColor, color } = colors.find((c) => c.name === name);
+
+  const buttonStyles = {
+    backgroundColor,
+    padding: "10px 45px",
+    border: "none",
+    borderRadius: 3,
+    margin: "2",
+    curser: "pointer",
+    fontSize: "1.25rem",
+    color,
+  };
+  return (props) => {
+    return <CopmParam {...props} style={buttonStyles} />;
+  };
+};
+
+const NewButton = buttonWithStyles(Button);
+const ReactButton = buttonWithStyles(Button, "react");
+const InfoButton = buttonWithStyles(Button, "info");
+const SuccessButton = buttonWithStyles(Button, "success");
+const WarningButton = buttonWithStyles(Button, "warning");
+const DangerButton = buttonWithStyles(Button, "danger");
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button
+          text="No Style"
+          onClick={() => alert("There is no style yet")}
+        />
+        <NewButton
+          text="Styled Button"
+          onClick={() => alert("I am the default style")}
+        />
+        <SuccessButton
+          text="Success"
+          onClick={() => alert("I am Successfull")}
+        />
+        <ReactButton text="React" onClick={() => alert("React styled")} />
+        <InfoButton text="Info" onClick={() => alert("I am with Info color")} />
+        <WarningButton text="Warning" onClick={() => alert("Warning styled")} />
+        <DangerButton
+          text="Danger"
+          onClick={() => alert("Danger, you can't go back")}
+        />
+      </div>
+    );
+  }
+}
+
 const rootElement = document.getElementById("root");
-ReactDom.render(<App />, rootElement);
+ReactDOM.render(<App />, rootElement);
